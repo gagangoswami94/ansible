be24791137d82d1f8ddbaba848add978a43869fd
Gagan Goswami <gagangiri94@gmail.com>
2022-09-10 13:30:27 +0530

    adding authorised keys

--- lab-setup/host_setup/ssh.sh
+++ lab-setup/host_setup/ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Adding master public key to authorized_keys on host to allow SSH connections
-cat /var/ans/master_key.pub >> /root/.ssh/authorized_keys
+cat /var/ansible/master_key.pub >> /root/.ssh/authorized_keys
 
 # Starting SSH server
 /usr/sbin/sshd -D
